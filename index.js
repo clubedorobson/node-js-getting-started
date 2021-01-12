163cb59eb665978cf11b8438c4376ea0b043df07
clubedorobson <clubedorobson@gmail.com>
2021-01-11 23:40:12 -0300

    New packages

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const cool = require('cool-ascii-faces');
+const axios = require('axios');
+const { JSDOM } = require("jsdom");
+const { window } = new JSDOM("");
+const $ = require("jquery")(window);
+const logger = require("logger").createLogger('development.log');
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -9,4 +22,27 @@ express()
   .set('view engine', 'ejs')
   .get('/cool', (req, res) => res.send(cool()))
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null };
+      res.render('pages/db', results);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+  .get('/proclubs', async (req, res) => {
+    axios.get('https://www.ea.com/pt-br/games/fifa/fifa-21/pro-clubs/ps4-xb1-pc/overview?clubId=6703918&platform=ps4')
+      .then(response => {
+        logger.warn(response)
+        console.log(response.data.url);
+        console.log(response.data.explanation);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))

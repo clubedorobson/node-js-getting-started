2888dc5a73f16107ed4c3b3b5dfccb6e6b7c3392
clubedorobson <clubedorobson@gmail.com>
2021-01-23 16:31:12 -0300

    CORS ALLOW DOMAINS

--- index.js
+++ index.js
@@ -14,7 +14,22 @@ var corsOptions = {
 };
 const path = __dirname + '/app/views/';
 
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
+
 app = express()
+app.use(allowCrossDomain)
 app.use(cors(corsOptions));
 app.use(express.static(path));
 app.get('/', function (req,res) {

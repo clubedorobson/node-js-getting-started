67061bff6943e879d45a7d81c0b08d18247e8d34
clubedorobson <clubedorobson@gmail.com>
2021-01-23 02:20:15 -0300

    Mudanças nos componentes Membro e MembrosList, bem como sua interligação

--- front-end/src/components/Membro.vue
+++ front-end/src/components/Membro.vue
@@ -63,13 +63,13 @@
                 :chartData="chartData"
                 :options="chartOptions"
               />
-              <b-button class="button-right" squared variant="primary" id="show-btn" @click="showModal">Detalhes</b-button>
+              <b-btn  variant="purple" class="button-right" id="show-btn" @click="showModal">Detalhes</b-btn>
             </b-card>
             
           </b-col>
           <b-col>
-            <b-card title="Estatísticas" fluid>
-              <b-container>
+            <b-card title="Estatísticas" >
+              
                 <b-row>
                   <b-col> Jogos </b-col>
                   <b-col>
@@ -90,7 +90,7 @@
                 </b-row>
                 <b-row>
                   <b-col> Assistências </b-col>
-                                    <b-col>
+                  <b-col>
                     <img width=20px src="../assets/football-icons/assistencia1.svg"/>
                   </b-col>
                   <b-col>
@@ -146,14 +146,110 @@
                   </b-col>
                   <b-col> {{ currentMembro.winRate }} % </b-col>
                 </b-row>
-              </b-container>
+              
             </b-card>
           </b-col>
         </b-row>
       </div>
     </b-card>
 
-    <b-modal ref="my-modal" >Hello From Modal!</b-modal>
+    <b-modal ref="my-modal" ok-only>
+      <b-row>
+        <b-col>
+          <b-card fluid>
+            <strong>Ritmo <span>{{statsMembro.geralPace}}</span> </strong>
+            <p></p>
+            <span class="statsName">Aceleração</span> <span class="stats">{{statsMembro.aceleracao}}</span>
+            <br>
+            <span class="statsName">Pique</span> <span class="stats">{{statsMembro.pique}}</span>
+          </b-card>
+        </b-col>
+        <b-col>
+          <b-card>
+            <strong>Finalização {{statsMembro.geralFinishing}}</strong>
+            <p></p>
+            <span class="statsName">Chute</span> <span class="stats">{{statsMembro.finalizacao}}</span>
+            <br>
+            <span class="statsName">Cobrança de falta</span><span span class="stats">{{statsMembro.aceleracao}}</span>
+            <br>
+            <span class="statsName">Cabeceio</span><span class="stats">{{statsMembro.cabeceio}}</span>
+            <br>
+            <span class="statsName">Força do chute</span><span class="stats">{{statsMembro.forcachute}}</span>
+            <br>
+            <span class="statsName">Chutes de longe</span><span class="stats">{{statsMembro.chuteslonge}}</span>
+            <br>
+            <span class="statsName">Voleio</span><span class="stats">{{statsMembro.voleio}}</span>
+            <br>
+            <span class="statsName">Penâltis</span><span class="stats">{{statsMembro.penaltis}}</span>
+          </b-card>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <b-card>
+            <strong>Passe {{statsMembro.geralPassing}}</strong>
+            <p></p>
+            <span class="statsName">Visão</span> <span class="stats">{{statsMembro.visao}}</span>
+            <br>
+            <span class="statsName">Cruzamento</span><span span class="stats">{{statsMembro.cruzamento}}</span>
+            <br>
+            <span class="statsName">Lançamento</span><span class="stats">{{statsMembro.lancamento}}</span>
+            <br>
+            <span class="statsName">Passe curto</span><span class="stats">{{statsMembro.passecurto}}</span>
+            <br>
+            <span class="statsName">Curva</span><span class="stats">{{statsMembro.curva}}</span>
+          </b-card>
+        </b-col>
+        <b-col>
+          <b-card>
+            <strong>Condução {{statsMembro.geralDribbling}}</strong>
+            <p></p>
+            <span class="statsName">Agilidade</span> <span class="stats">{{statsMembro.agilidade}}</span>
+            <br>
+            <span class="statsName">Equilíbrio</span><span span class="stats">{{statsMembro.equilibrio}}</span>
+            <br>
+            <span class="statsName">Posicionamento</span><span class="stats">{{statsMembro.posAtaque}}</span>
+            <br>
+            <span class="statsName">Controle de bola</span><span class="stats">{{statsMembro.controlebola}}</span>
+            <br>
+            <span class="statsName">Condução</span><span class="stats">{{statsMembro.conducao}}</span>
+          </b-card>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <b-card>
+            <strong>Defesa {{statsMembro.geralDefending}}</strong>
+            <p></p>
+            <span class="statsName">Interceptação</span> <span class="stats">{{statsMembro.interceptacao}}</span>
+            <br>
+            <span class="statsName">Marcação</span><span span class="stats">{{statsMembro.nocaodefensiva}}</span>
+            <br>
+            <span class="statsName">Dividida</span><span class="stats">{{statsMembro.divididaempe}}</span>
+            <br>
+            <span class="statsName">Carrinho</span><span class="stats">{{statsMembro.carrinho}}</span>
+          </b-card>
+        </b-col>
+        <b-col>
+          <b-card>
+            <strong>Físico {{statsMembro.geralPhysical}}</strong>
+            <p></p>
+            <span class="statsName">Impulsão</span> <span class="stats">{{statsMembro.impulsao}}</span>
+            <br>
+            <span class="statsName">Fôlego</span><span span class="stats">{{statsMembro.folego}}</span>
+            <br>
+            <span class="statsName">Força</span><span class="stats">{{statsMembro.forca}}</span>
+            <br>
+            <span class="statsName">Reação</span><span class="stats">{{statsMembro.reacao}}</span>
+            <br>
+            <span class="statsName">Combatividade</span><span class="stats">{{statsMembro.combatividade}}</span>
+            <br>
+            <span class="statsName">Frieza</span><span class="stats">{{statsMembro.frieza}}</span>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-modal>
+
   </div>
 </template>
 
@@ -207,6 +303,7 @@ export default {
         title: {
           display: false,
         },
+        backgroundColor : 'rgba(225, 21, 232, 1)' 
       },
       chartData: {},
     };
@@ -216,149 +313,150 @@ export default {
       this.$refs['my-modal'].show()
     },
     async getMembro(id, clubId) {
-      membrosService
-        .get(id, clubId)
-        .then((response) => {
-          this.currentMembro = response.data[0];
-          console.log("MEMBRO::", response.data[0]);
-          this.arrayStats = this.currentMembro.membrosMembrosPartidas[0].vproattr.split(
-            "|"
-          );
-          console.log(this.arrayStats);
-          this.statsMembro = {
-            aceleracao: parseInt(this.arrayStats[0]),
-            pique: parseInt(this.arrayStats[1]),
-            agilidade: parseInt(this.arrayStats[2]),
-            equilibrio: parseInt(this.arrayStats[3]),
-            impulsao: parseInt(this.arrayStats[4]),
-            folego: parseInt(this.arrayStats[5]),
-            forca: parseInt(this.arrayStats[6]),
-            reacao: parseInt(this.arrayStats[7]),
-            combatividade: parseInt(this.arrayStats[8]),
-            frieza: parseInt(this.arrayStats[9]),
-            interceptacao: parseInt(this.arrayStats[10]),
-            posAtaque: parseInt(this.arrayStats[11]),
-            visao: parseInt(this.arrayStats[12]),
-            chuteslonge: parseInt(this.arrayStats[13]),
-            forcachute: parseInt(this.arrayStats[14]),
-            conducao: parseInt(this.arrayStats[15]),
-            finalizacao: parseInt(this.arrayStats[16]),
-            cobrancafalta: parseInt(this.arrayStats[17]),
-            cabeceio: parseInt(this.arrayStats[18]),
-            lancamento: parseInt(this.arrayStats[19]),
-            passecurto: parseInt(this.arrayStats[20]),
-            nocaodefensiva: parseInt(this.arrayStats[21]),
-            cruzamento: parseInt(this.arrayStats[22]),
-            controlebola: parseInt(this.arrayStats[23]),
-            divididaempe: parseInt(this.arrayStats[24]),
-            carrinho: parseInt(this.arrayStats[25]),
-            voleio: parseInt(this.arrayStats[26]),
-            curva: parseInt(this.arrayStats[27]),
-            penaltis: parseInt(this.arrayStats[28]),
-            goleiro: parseInt(this.arrayStats[29]),
-            mergulho: parseInt(this.arrayStats[30]),
-            manejo: parseInt(this.arrayStats[31]),
-            chutegoleiro: parseInt(this.arrayStats[32]),
-            reflexos: parseInt(this.arrayStats[33]),
-          };
+      let response = await membrosService.get(id, clubId)
+      this.currentMembro = response.data[0]
+      console.log("ROBSONER", this.robsoner)
+      console.log("MEMBRO::", this.currentMembro);
+      if (this.robsoner.isfake) {
+        this.arrayStats = this.robsoner.vproattr.split("|");
+        this.currentMembro = this.robsoner
+      }
+      else { 
+        this.arrayStats = this.currentMembro.membrosMembrosPartidas[0].vproattr.split("|");
+      }
+      console.log(this.arrayStats);
 
-          this.statsMembro["geralFinishing"] =
-            this.statsMembro.finalizacao * 0.45 +
-            this.statsMembro.chuteslonge * 0.2 +
-            this.statsMembro.forcachute * 0.2 +
-            this.statsMembro.posAtaque * 0.05 +
-            this.statsMembro.penaltis * 0.05 +
-            this.statsMembro.voleio * 0.05;
+      this.statsMembro = {
+        aceleracao: parseInt(this.arrayStats[0]),
+        pique: parseInt(this.arrayStats[1]),
+        agilidade: parseInt(this.arrayStats[2]),
+        equilibrio: parseInt(this.arrayStats[3]),
+        impulsao: parseInt(this.arrayStats[4]),
+        folego: parseInt(this.arrayStats[5]),
+        forca: parseInt(this.arrayStats[6]),
+        reacao: parseInt(this.arrayStats[7]),
+        combatividade: parseInt(this.arrayStats[8]),
+        frieza: parseInt(this.arrayStats[9]),
+        interceptacao: parseInt(this.arrayStats[10]),
+        posAtaque: parseInt(this.arrayStats[11]),
+        visao: parseInt(this.arrayStats[12]),
+        chuteslonge: parseInt(this.arrayStats[13]),
+        forcachute: parseInt(this.arrayStats[14]),
+        conducao: parseInt(this.arrayStats[15]),
+        finalizacao: parseInt(this.arrayStats[16]),
+        cobrancafalta: parseInt(this.arrayStats[17]),
+        cabeceio: parseInt(this.arrayStats[18]),
+        lancamento: parseInt(this.arrayStats[19]),
+        passecurto: parseInt(this.arrayStats[20]),
+        nocaodefensiva: parseInt(this.arrayStats[21]),
+        cruzamento: parseInt(this.arrayStats[22]),
+        controlebola: parseInt(this.arrayStats[23]),
+        divididaempe: parseInt(this.arrayStats[24]),
+        carrinho: parseInt(this.arrayStats[25]),
+        voleio: parseInt(this.arrayStats[26]),
+        curva: parseInt(this.arrayStats[27]),
+        penaltis: parseInt(this.arrayStats[28]),
+        goleiro: parseInt(this.arrayStats[29]),
+        mergulho: parseInt(this.arrayStats[30]),
+        manejo: parseInt(this.arrayStats[31]),
+        chutegoleiro: parseInt(this.arrayStats[32]),
+        reflexos: parseInt(this.arrayStats[33]),
+      };
 
-          this.statsMembro["geralFinishing"] = Math.round(
-            this.statsMembro["geralFinishing"]
-          );
+      this.statsMembro["geralFinishing"] =
+        this.statsMembro.finalizacao * 0.45 +
+        this.statsMembro.chuteslonge * 0.2 +
+        this.statsMembro.forcachute * 0.2 +
+        this.statsMembro.posAtaque * 0.05 +
+        this.statsMembro.penaltis * 0.05 +
+        this.statsMembro.voleio * 0.05;
 
-          this.statsMembro["geralPace"] =
-            this.statsMembro.pique * 0.55 + this.statsMembro.aceleracao * 0.45;
+      this.statsMembro["geralFinishing"] = Math.round(
+        this.statsMembro["geralFinishing"]
+      );
 
-          this.statsMembro["geralPace"] = Math.round(
-            this.statsMembro["geralPace"]
-          );
+      this.statsMembro["geralPace"] =
+        this.statsMembro.pique * 0.55 + this.statsMembro.aceleracao * 0.45;
 
-          this.statsMembro["geralPassing"] =
-            this.statsMembro.passecurto * 0.35 +
-            this.statsMembro.visao * 0.2 +
-            this.statsMembro.cruzamento * 0.2 +
-            this.statsMembro.lancamento * 0.15 +
-            this.statsMembro.curva * 0.05 +
-            this.statsMembro.cobrancafalta * 0.05;
+      this.statsMembro["geralPace"] = Math.round(
+        this.statsMembro["geralPace"]
+      );
 
-          this.statsMembro["geralPassing"] = Math.round(
-            this.statsMembro["geralPassing"]
-          );
+      this.statsMembro["geralPassing"] =
+        this.statsMembro.passecurto * 0.35 +
+        this.statsMembro.visao * 0.2 +
+        this.statsMembro.cruzamento * 0.2 +
+        this.statsMembro.lancamento * 0.15 +
+        this.statsMembro.curva * 0.05 +
+        this.statsMembro.cobrancafalta * 0.05;
 
-          this.statsMembro["geralDribbling"] =
-            this.statsMembro.conducao * 0.5 +
-            this.statsMembro.controlebola * 0.35 +
-            this.statsMembro.agilidade * 0.1 +
-            this.statsMembro.equilibrio * 0.05;
+      this.statsMembro["geralPassing"] = Math.round(
+        this.statsMembro["geralPassing"]
+      );
 
-          this.statsMembro["geralDribbling"] = Math.round(
-            this.statsMembro["geralDribbling"]
-          );
+      this.statsMembro["geralDribbling"] =
+        this.statsMembro.conducao * 0.5 +
+        this.statsMembro.controlebola * 0.35 +
+        this.statsMembro.agilidade * 0.1 +
+        this.statsMembro.equilibrio * 0.05;
 
-          this.statsMembro["geralDefending"] =
-            this.statsMembro.nocaodefensiva * 0.3 +
-            this.statsMembro.divididaempe * 0.3 +
-            this.statsMembro.interceptacao * 0.2 +
-            this.statsMembro.cabeceio * 0.1 +
-            this.statsMembro.carrinho * 0.1;
+      this.statsMembro["geralDribbling"] = Math.round(
+        this.statsMembro["geralDribbling"]
+      );
 
-          this.statsMembro["geralDefending"] = Math.round(
-            this.statsMembro["geralDefending"]
-          );
+      this.statsMembro["geralDefending"] =
+        this.statsMembro.nocaodefensiva * 0.3 +
+        this.statsMembro.divididaempe * 0.3 +
+        this.statsMembro.interceptacao * 0.2 +
+        this.statsMembro.cabeceio * 0.1 +
+        this.statsMembro.carrinho * 0.1;
 
-          this.statsMembro["geralPhysical"] =
-            this.statsMembro.forca * 0.5 +
-            this.statsMembro.folego * 0.25 +
-            this.statsMembro.combatividade * 0.2 +
-            this.statsMembro.impulsao * 0.05;
+      this.statsMembro["geralDefending"] = Math.round(
+        this.statsMembro["geralDefending"]
+      );
+
+      this.statsMembro["geralPhysical"] =
+        this.statsMembro.forca * 0.5 +
+        this.statsMembro.folego * 0.25 +
+        this.statsMembro.combatividade * 0.2 +
+        this.statsMembro.impulsao * 0.05;
 
           this.statsMembro["geralPhysical"] = Math.round(
             this.statsMembro["geralPhysical"]
           );
 
-          this.chartData["datasets"] = [];
-          this.chartData["datasets"].push({
-            data: [
-              this.statsMembro["geralFinishing"],
-              this.statsMembro["geralPassing"],
-              this.statsMembro["geralDribbling"],
-              this.statsMembro["geralDefending"],
-              this.statsMembro["geralPhysical"],
-              this.statsMembro["geralPace"],
-            ],
-          });
-
-          this.chartData.datasets[0].backgroundColor = "rgba(255, 0, 0, 0.1)";
-          this.chartData.datasets[0].label = "";
-
-          console.log(this.statsMembro);
-          this.chartData.labels = [
-            "Finalização",
-            "Passe",
-            "Condução",
-            "Defesa",
-            "Físico",
-            "Ritmo",
-          ];
-          console.log(this.chartData);
-
-          this.positions.forEach((pos, i) => {
-            if (i == this.currentMembro.proPos) {
-              this.robsoner["position"] = pos;
-            }
-          });
-        })
-        .catch((e) => {
-          console.log(e);
+        this.chartData["datasets"] = [];
+        this.chartData["datasets"].push({
+          backgroundColor : 'rgba(225, 21, 232, 0.1)',
+          label: '',
+          data: [
+            this.statsMembro["geralFinishing"],
+            this.statsMembro["geralPassing"],
+            this.statsMembro["geralDribbling"],
+            this.statsMembro["geralDefending"],
+            this.statsMembro["geralPhysical"],
+            this.statsMembro["geralPace"],
+          ],
         });
+
+      console.log(this.statsMembro);
+      this.chartData.labels = [
+        "Finalização",
+        "Passe",
+        "Condução",
+        "Defesa",
+        "Físico",
+        "Ritmo",
+      ];
+
+      console.log(this.chartData);
+
+      this.positions.forEach((pos, i) => {
+        if (i == this.currentMembro.proPos) {
+          this.robsoner["position"] = pos;
+        }
+      });
+
     },
 
     async getRobsoner(name) {
@@ -378,12 +476,17 @@ export default {
     await this.getMembro(this.$route.params.id, this.$route.params.clubId);
   },
 };
+
 </script>
 
 <style>
 .edit-form {
   max-width: 300px;
   margin: auto;
+}
+
+div.col{
+  text-align: center !important;
 }
 
 .btable {
@@ -416,4 +519,33 @@ export default {
 .button-right{
   float: right
 }
+
+.stats {
+  float: right;
+}
+
+.statsName {
+  float: left;
+}
+
+.vGood {
+    background-color: #2bde07;
+}
+
+.good {
+    background-color: #9ddb0b;
+}
+
+.avg {
+    background-color: #ffff00;
+}
+
+.poor {
+    background-color: #fa9214;
+}
+
+.vPoor {
+    background-color: #ff2519;
+}
+
 </style>
--- front-end/src/components/MembrosList.vue
+++ front-end/src/components/MembrosList.vue
@@ -1,88 +1,123 @@
 <template>
-  <div class="list row">
-    <!--<div class="col-md-8">
-      <div class="input-group mb-3">
-        <input type="text" class="form-control" placeholder="Search by title"
-          v-model="title"/>
-        <div class="input-group-append">
-          <button class="btn btn-outline-secondary" type="button"
-            @click="searchTitle"
-          >
-            Search
-          </button>
-        </div>
-      </div>
-    </div>-->
-    <div class="col-md-6">
-      <h4>Membros</h4>
-      <ul class="list-group">
-        <li class="list-group-item"
-          :class="{ active: index == currentIndex }"
-          v-for="(membro, index) in membros"
-          :key="index"
-          @click="setActiveMembro(membro, index)"
-        >
-          {{ membro.proName }}
-        </li>
-      </ul>
+  <div>
+    <v-data-table v-if="listMembros" :items="listMembros" :headers="headers">
+      <template v-slot:item.face="{ item }">
+        <b-img center rounded="circle" height="100%" width="100%" :src="item.face" alt="..." />
+      </template>
 
-      <!--<button class="m-3 btn btn-sm btn-danger" @click="removeAllTutorials">
-        Remove All
-      </button>-->
-    </div>
-    <div class="col-md-6">
-      <div v-if="currentMembro">
-        <h4>Membro</h4>
-        <div>
-          <label><strong>Nome:</strong></label> {{ currentMembro.favoritePosition }}
-        </div>
-        <div>
-          <label><strong>Gols:</strong></label> {{ currentMembro.goals }}
-        </div>
-        <div>
-          <label><strong>Assistencias:</strong></label> {{ currentMembro.assists}}
-        </div>
-
-        <a class="badge badge-warning"
-          :href="`/membros/${this.$route.params.clubId}/` + currentMembro.name"
+      <template v-slot:item.name="{ item }">
+        <b-btn
+          :title="Detalhes"
+          variant="purple"
+          :to="{
+            name: 'membro',
+            params: { id: item.name },
+          }"
+          >Detalhes</b-btn
         >
-          Edit
-        </a>
-      </div>
-      <div v-else>
-        <br />
-        <p>Please click on a Tutorial...</p>
-      </div>
-    </div>
+      </template>
+    </v-data-table>
   </div>
 </template>
 
 <script>
 import membrosService from "../services/membrosService";
+import robsonService from "../services/robsonService";
 
 export default {
   name: "membros-list",
   data() {
     return {
       membros: [],
+      robsoners: [],
+      listMembros: [],
+      fakeRobsoners: [],
       currentMembro: null,
       currentIndex: -1,
-      title: ""
+      title: "",
+      headers: [
+        {
+          value: "face",
+          text: "",
+          sortable: false,
+        },
+        {
+          value: "number",
+          text: "",
+          sortable: false,
+        },
+        {
+          value: "proName",
+          text: "",
+          sortable: false,
+        },
+        {
+          value: "name",
+          text: "",
+          sortable: false,
+        },
+      ],
     };
   },
   methods: {
-
-    getMembros(clubId) {
-      membrosService.getAll(clubId)
-        .then(response => {
+    async getMembros(clubId) {
+      membrosService
+        .getAll(clubId)
+        .then((response) => {
           this.membros = response.data;
           console.log(response.data);
+          robsonService.getAll().then((response) => {
+            this.robsoners = response.data;
+            console.log(response.data);
+
+            this.membros.forEach((membro, i) => {
+              let face = this.robsoners.find(
+                (robsoner) => robsoner.name == membro.name
+              );
+              if (face == undefined) {
+                this.membros[i]["face"] = "https://banner2.cleanpng.com/20180603/jx/kisspng-user-interface-design-computer-icons-default-stephen-salazar-photography-5b1462e1b19d70.1261504615280626897275.jpg";
+              } else {
+                this.membros[i]["number"] = face.number;
+                this.membros[i]["face"] = face.face;
+              }
+            });
+
+            console.log(this.membros);
+
+            this.listMembros = this.membros;
+
+            console.log(this.robsoners)
+
+            this.robsoners.forEach(robsoner => {
+              if(robsoner.isfake) {
+                this.listMembros.push(robsoner)  
+              }
+            })
+
+            console.log(this.listMembros);
+          });
         })
-        .catch(e => {
+        .catch((e) => {
           console.log(e);
         });
     },
 
+    async getRobsoners() {
+      robsonService.getAll().then((response) => {
+        this.robsoners = response.data;
+        console.log(response.data);
+        this.robsoners.forEach((robsoner) => {
+          this.membros.forEach((membro, i) => {
+            if (membro.name == robsoner.name) {
+              this.membros[i]["face"] = robsoner.face;
+            } else {
+              this.membros[i]["face"] = "https://funkyimg.com/i/3aepK.png";
+            }
+          });
+        });
+        console.log(this.membros);
+      });
+    },
     /*refreshList() {
       this.retrieveTutorials();
       this.currentTutorial = null;
@@ -94,9 +129,9 @@ export default {
       this.currentIndex = index;
     },
   },
-  mounted() {
-    this.getMembros(this.$route.params.clubId);
-  }
+  async mounted() {
+    await this.getMembros(this.$route.params.clubId);
+  },
 };
 </script>
 
@@ -105,5 +140,9 @@ export default {
   text-align: left;
   max-width: 750px;
   margin: auto;
+}
+
+td {
+  text-align: center !important
 }
 </style>

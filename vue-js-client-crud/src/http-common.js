743c4e92a6195750c039d3edc3ed76af3a462e67
clubedorobson <clubedorobson@gmail.com>
2021-01-23 23:40:13 -0300

    Clean vue-js folder

--- vue-js-client-crud/src/http-common.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "http://clube-do-robson.herokuapp.com:5000/api",
-  headers: {
-    "Content-type": "application/json"
-  }
-});

4c087db9a2e5fd8b8f7be0171558b9eefb94750f
clubedorobson <clubedorobson@gmail.com>
2021-01-24 00:38:26 -0300

    Updated cron time to 30min

--- schedule.js
+++ schedule.js
@@ -396,7 +396,7 @@ async function updateClub(clubId) {
   
   
   
-  cron.schedule('*/5 * * * *', async () => {
+  cron.schedule('*/30 * * * *', async () => {
     logger.info("RUNNING CRON")
     await updateMatches();
   })
